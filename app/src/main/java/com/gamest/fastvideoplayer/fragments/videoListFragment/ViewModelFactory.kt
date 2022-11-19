1ab87c5d42a249d0c5cc4b83b336bd93fc98d571
somesh <somesh.verma@career360.com>
2022-11-19 13:28:56 +0530

    fixed ui

--- app/src/main/java/com/gamest/fastvideoplayer/fragments/videoListFragment/ViewModelFactory.kt
+++ app/src/main/java/com/gamest/fastvideoplayer/fragments/videoListFragment/ViewModelFactory.kt
@@ -5,7 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ViewModelFactory(private val contentResolver:ContentResolver?): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(VideoListViewModel::class.java))
             return VideoListViewModel(contentResolver) as T
         else
